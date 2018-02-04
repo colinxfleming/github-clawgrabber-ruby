e886f2c4b98fce15586523e82212cd5fd3b94f66
colinxfleming <c3flemin@gmail.com>
2018-02-04 01:49:25 -0500

    write basic github fetcher... WITH GRAPHQL

--- lib/github_clawgrabber.rb
+++ lib/github_clawgrabber.rb
@@ -1,5 +1,26 @@
-require "github_clawgrabber/version"
+require 'github_clawgrabber/version'
+require 'github_clawgrabber/authentciator'
+require 'github_clawgrabber/fetcher'
+require 'github_clawgrabber/templater'
 
 module GithubClawgrabber
-  # Your code goes here...
+  def self.grab(auth_token, repo, filepath, branch = 'master')
+    content = fetch auth_token, repo, filepath, branch
+    template
+    output
+  end
+
+  private
+
+  def fetch
+    Fetcher.something
+  end
+
+  def template
+    Templater.something
+  end
+
+  def output
+    Outputter.something
+  end
 end
--- lib/github_clawgrabber/version.rb
+++ lib/github_clawgrabber/version.rb
@@ -1,3 +1,3 @@
 module GithubClawgrabber
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'
 end
