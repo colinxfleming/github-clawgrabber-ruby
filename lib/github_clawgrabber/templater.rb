fde5b73578a32878d9435f29ee60ebf4b58dbc07
colinxfleming <c3flemin@gmail.com>
2018-02-05 15:38:56 -0500

    working context modules

--- lib/github_clawgrabber/templater.rb
+++ lib/github_clawgrabber/templater.rb
@@ -3,20 +3,34 @@ require 'erb'
 module GithubClawgrabber
   class Templater
     class << self
-      def template(content, context = nil)
+      def template(content, *context_modules)
         content = ERB.new content
 
-        b = apply_context context
+        b = apply_context context_modules
         content.result(b)
       end
 
       private
 
-      def apply_context(context)
-        return if context.nil?
+      def apply_context(context_modules)
+        return if context_modules.empty?
 
-        context.binding
+        context_shell = ContextClass.new context_modules
+        context_shell.access_binding
       end
+    end
+  end
+
+  # a dummy class to hold our modules
+  class ContextClass
+    def initialize(modules)
+      modules.each do |mod|
+        self.class.send :include, mod
+      end
+    end
+
+    def access_binding
+      binding
     end
   end
 end
