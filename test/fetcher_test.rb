7d1142b174a6bcd8fdc5989d27e79cf24bb10669
colinxfleming <c3flemin@gmail.com>
2018-02-06 22:58:01 -0500

    for later

--- test/fetcher_test.rb
+++ test/fetcher_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 # Test that the context shell initializes and makes binding accessible
 class FetcherTest < TestClass
+  before { stub_github }
+
   describe 'constants' do
     it 'should set the GraphQL endpoint' do
       assert_equal 'https://api.github.com/graphql',
@@ -10,10 +12,6 @@ class FetcherTest < TestClass
   end
 
   describe 'fetching' do
-    before do
-      stub_github
-    end
-
     it 'makes a single file call' do
       response = GithubClawgrabber::Fetcher.fetch(
         'valid_token',
@@ -25,13 +23,25 @@ class FetcherTest < TestClass
       assert_equal 'samples/each_loop.txt.erb', response[0][:file]
       assert_equal "<% [1, 2, 3, 4, 5].each do |num| %>\n  <%= num %> * 2 = <%= num * 2 %>\n<\% end %>\n",
                    response[0][:content]
-
     end
   end
 
-  # describe 'private methods' do
-  #     assert_equal 200, response.code
-  #     assert_equal SUCCESSFUL_CALL_HEADERS, response.headers
-  #     assert_equal SUCCESSFUL_CALL_BODY, response.body
-  # end
+  describe 'private methods' do
+    it 'makes clean api calls' do
+      response = GithubClawgrabber::Fetcher.send(
+        :make_api_call,
+        'valid_token',
+        'colinxfleming/github_clawgrabber_ruby',
+        'samples/each_loop.txt.erb',
+        'master'
+      )
+
+      assert_equal 200, response.code
+      assert_equal SUCCESSFUL_CALL_HEADERS, response.headers
+      assert_equal SUCCESSFUL_CALL_BODY, response.body
+    end
+
+    # TODO test header setting
+    # TODO test shaping post json
+  end
 end
